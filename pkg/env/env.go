217c359a82974b3b91f29b8b523c57dc4adbf367
domain <do@my-sh.dev>
2023-12-12 11:02:08 +0800

    :sparkles: Add env parser.

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -1,13 +1,22 @@
 package env
 
-const (
-	Name             = "ttyd"
-	Ramdisk          = "/usr/ramdisk"
-	ExtensionHome    = Ramdisk + "/app"
-	ExtensionDir     = ExtensionHome + "/" + Name
-	ExtensionLibDir  = ExtensionDir + "/lib"
-	ControlPanelHome = Ramdisk + "/admin"
+import (
+	"bufio"
+	"os"
+	"strings"
+)
 
+const (
+	Name           = "ttyd"
+	Environment    = "/etc/PG.conf"
+	PanabitPathKey = "PGPATH"
+
+	Ramdisk         = "/usr/ramdisk"
+	ExtensionHome   = Ramdisk + "/app"
+	ExtensionDir    = ExtensionHome + "/" + Name
+	ExtensionLibDir = ExtensionDir + "/lib"
+
+	ControlPanelHome      = Ramdisk + "/admin"
 	ExtensionCgiDir       = ControlPanelHome + "/cgi-bin/App/" + Name
 	ExtensionWebAssetsDir = ControlPanelHome + "/html/App/" + Name
 )
@@ -20,8 +29,33 @@ var (
 )
 
 func Init() {
-	// TODO assign from /etc/PG.conf
+	StorageHome = "/usr/panabit"
+	if v, err := find(PanabitPathKey); err != nil && v != "" {
+		StorageHome = v
+	}
 	ExtensionStorageDir = StorageHome + "/app/" + Name
 	ExtensionCgiStorageDir = ExtensionStorageDir + "/web/cgi"
 	ExtensionWebAssetsStorageDir = ExtensionStorageDir + "/web/html"
+}
+
+func find(key string) (string, error) {
+	f, err := os.Open(Environment)
+	if err != nil {
+		return "", err
+	}
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		t := sc.Text()
+		if strings.HasPrefix(key, t) {
+			if w := strings.Split(t, "="); len(w) == 2 {
+				return w[1], nil
+			} else {
+				return "", nil
+			}
+		}
+	}
+	if err := sc.Err(); err != nil {
+		return "", err
+	}
+	return "", err
 }
